dfd4b8cfbdb9e214a944ba70c8ec2cbd0829bec0
Joshua Magady <josh.magady@gmail.com>
2020-04-19 13:52:57 -0500

    Creat copyfiles.sh

---
+++ copyfiles.sh
@@ -0,0 +1,6 @@
+# The -u option ensures that only newer versions from source will replace existing files in destination. See https://superuser.com/a/341296.
+# The -a option, AKA "archive", recursively copies directories within source while preserving time stamps and permissions, etc.
+# The -T option ensures that we copy the contents of source into directory, not directory source itself.
+
+
+cp -uaT $SOURCE $DESTINATION
